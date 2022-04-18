5d04a193a941af73ea917b78e81ae6bded22295b
dnovick <davenovick@hotmail.com>
2022-04-18 10:52:01 -0700

    Did some Python data and method hiding through private and static data.

--- python/lincoln_high.py
+++ python/lincoln_high.py
@@ -4,9 +4,12 @@ import functools
 import operator
 import random
 
-# Generalized left fold.
-def foldl(func, acc, xs):
-    return functools.reduce(func, xs, acc)
+class utils():
+    # Generalized left fold.
+    @staticmethod
+    def foldl(func, acc, xs):
+        return functools.reduce(func, xs, acc)
+    
 
 # RSA encrypter/decrypter based on p, q, and e.
 class rsa_encrypter():
@@ -14,18 +17,18 @@ class rsa_encrypter():
         #TODO: Use CRT private keys to improve performance.
         #self.p = p
         #self.q = q
-        if not self.is_prime(p):
+        if not self.__is_prime(p):
             raise Exception("p is not prime.")
             
-        if not self.is_prime(q):
+        if not self.__is_prime(q):
             raise Exception("q is not prime.")
             
-        self.n = p * q
-        self.e = e
-        self.d = pow(e, -1, self.euler_phi(self.n))
+        self.__n = p * q
+        self.__e = e
+        self.__d = pow(e, -1, self.__euler_phi(self.__n))
         
-
-    def is_prime(self, n):
+    
+    def __is_prime(self, n):
         """"pre-condition: n is a nonnegative integer
         post-condition: return True if n is prime and False otherwise."""
         if n < 2: 
@@ -39,71 +42,71 @@ class rsa_encrypter():
              k += 2
         return True
 
-    def euler_phi(self, n):
+    def __euler_phi(self, n):
         y = n
         for i in range(2,n+1):
-            if self.is_prime(i) and n % i  == 0 :
+            if self.__is_prime(i) and n % i  == 0 :
                 y -= y/i
             else:
                 continue
         return int(y)
 
     def encrypt(self, msg):
-        if msg >= self.n:
+        if msg >= self.__n:
             raise Exception("Message is larger than RSA modulus.")
             
-        return pow(msg, self.e, self.n)
+        return pow(msg, self.__e, self.__n)
         
     def decrypt(self, ctext):
-        if ctext >= self.n:
+        if ctext >= self.__n:
             raise Exception("Ciphertext is larger than RSA modulus.")
-        return pow(ctext, self.d, self.n)
+        return pow(ctext, self.__d, self.__n)
 
 
 # Word encoder/decoder based on base 27 math.
 class word_encoder():
     
     def __init__(self):
-        self.base = 27
-        self.letterCodes = list(range(1, self.base))
-        self.asciiShift = 96
+        self.__base = 27
+        self.__letterCodes = list(range(1, self.__base))
+        self.__asciiShift = 96
 
-    def from_digits(self, digits):
+    def __from_digits(self, digits):
         l = len(digits)
-        mults = [pow(self.base, i) for i in list(range(0, l))]
+        mults = [pow(self.__base, i) for i in list(range(0, l))]
         mults.reverse()
-        return foldl(operator.add, 0,[d * m for d,m in zip(digits, mults)])
+        return utils.foldl(operator.add, 0,[d * m for d,m in zip(digits, mults)])
     
     def encode(self, w): 
         chars = [ord(c) for c in w.lower()]
-        digits = [c - self.asciiShift for c in chars]
-        return self.from_digits(digits)
+        digits = [c - self.__asciiShift for c in chars]
+        return self.__from_digits(digits)
    
-    def to_digits(self, n):
+    def __to_digits(self, n):
         if n == 0:
             return [0]
         digits = []
         while n:
-            digits.append(int(n % self.base))
-            n //= self.base
+            digits.append(int(n % self.__base))
+            n //= self.__base
         return digits[::-1]
 
     def decode(self, i):
-        digits = self.to_digits(i);
-        return ''.join([chr(d + self.asciiShift) for d in digits])
+        digits = self.__to_digits(i);
+        return ''.join([chr(d + self.__asciiShift) for d in digits])
     
 
 # Word encode and encrypt / decrypt and decode
 class word_encrypter():
     def __init__(self, p, q, e):
-        self.rsa = rsa_encrypter(p, q, e)
-        self.enc = word_encoder()
+        self.__rsa = rsa_encrypter(p, q, e)
+        self.__enc = word_encoder()
     
     def encode_and_encrypt(self, w): 
-        return self.enc.decode(self.rsa.encrypt(self.enc.encode(w)))
+        return self.__enc.decode(self.__rsa.encrypt(self.__enc.encode(w)))
     
     def decrypt_and_decode(self, w): 
-        return self.enc.decode(self.rsa.decrypt(self.enc.encode(w)))
+        return self.__enc.decode(self.__rsa.decrypt(self.__enc.encode(w)))
     
     def verify_word(self, ew, dw, words): 
         return (self.decrypt_and_decode(ew) == dw) and (dw in words)
@@ -111,9 +114,11 @@ class word_encrypter():
     
 # The word list with associated ciphertexts along with some verification, query, and extraction routines.    
 class word_list():
+    
+    
     def __init__(self, p, q, e):
-        self.enc = word_encrypter(p, q, e)
-        self.words = ["snit", "hype", "jock", "dude", "labs", "funk", "nosy", 
+        self.__enc = word_encrypter(p, q, e)
+        self.__words = ["snit", "hype", "jock", "dude", "labs", "funk", "nosy", 
                         "bomb", "math", "dare", "eeew", "prob", "foul", "tizz", "meow", 
                         "bark", "ditz", "dump", "goop", "guts", "lads", "gals", "icky", 
                         "yuch", "yeti", "wuss", "yawl", "zarf", "yups", "quip", "nill", 
@@ -147,47 +152,52 @@ class word_list():
                         "yolk", "yoke", "arid", "ooze", "walk", "coda"
                     ]
 
-        self.words.sort()
+        self.__words.sort()
             
-        self.cts = [self.enc.encode_and_encrypt(w) for w in self.words]
-            
-    def verify_word_list(self):
-        print("Word list is duplicate free: ", len(list(dict.fromkeys(self.words))) == len(self.words))
-
-        print("Word count: ", len(self.words))
+        self.__cts = [self.__enc.encode_and_encrypt(w) for w in self.__words]
+    
+    def verify_word(self, ew, dw):
+        return self.__enc.verify_word(ew, dw, self.__words)
         
-        dts = [self.enc.decrypt_and_decode(ct) for ct in self.cts]
-        print("Decryptions successful: ", dts == self.words)
+    def verify_word_list(self):
+        print("Word list is duplicate free: ", len(list(dict.fromkeys(self.__words))) == len(self.__words))
+
+        print("Word count: ", len(self.__words))
+        
+        dts = [self.__enc.decrypt_and_decode(ct) for ct in self.__cts]
+        print("Decryptions successful: ", dts == self.__words)
 
 
-        matched = foldl(operator.and_, 
+        matched = utils.foldl(operator.and_, 
                         True, 
-                        [self.enc.verify_word(self.cts[i], self.words[i], self.words) for i in range(0, len(self.words))]
+                        [
+                            self.verify_word(self.__cts[i], self.__words[i]) for i in range(0, len(self.__words))
+                        ]
                        )
         print("Lists match: ", matched)
     
     def print_word_list(self):
-        [print(w) for w in self.words]
+        [print(w) for w in self.__words]
         
     def write_word_list(self, filepath):
         with open(filepath, 'w') as f:
-            [f.write(w + "\n\n") for w in self.words]
+            [f.write(w + "\n\n") for w in self.__words]
             
     def print_ciphertext_list(self):
-        [print(ct) for ct in self.cts]
+        [print(ct) for ct in self.__cts]
     
     def write_ciphertext_list(self, filepath):
          with open(filepath, 'w') as f:
-            [f.write(ct + "\n\n") for ct in self.cts]
+            [f.write(ct + "\n\n") for ct in self.__cts]
          
     
     def word_in_words(self, w):
-        return w in self.words
+        return w in self.__words
     
     def ct_in_ciphertexts(self, ct):
-        return ct in self.cts
+        return ct in self.__cts
     
-
+    
 #### Example usage
 # import lincoln_high
 # import operator
